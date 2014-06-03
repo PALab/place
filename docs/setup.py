f072dd867cb9545629b6c84c847577714c8b9ea4
johjam <jami.johnson@auckland.ac.nz>
2014-06-03 17:11:43 +1200

    Update setup.py

--- docs/setup.py
+++ docs/setup.py
@@ -1,11 +1,15 @@
+import os
+import sys
+import inspect
 from distutils.core import setup
+
 
 #update URL
 #license
 setup(
     name='PyPAL',
     version='0.1.0',
-    author='Jami L. Johnson, Kasper van Wijk, Henrik tom Wörden',
+    author='Jami L. Johnson, Kasper van Wijk, Henrik tom Worden',
     author_email='jami.johnson@auckland.ac.nz',
     packages=['pypal','pypal.analysis','pypal.automate','pypal.automate.osci_card','pypal.automate.polytec','pypal.automate.tektronix','pypal.automate.xps_control'],
     scripts=['bin/Scan.py','bin/example_PALplots.py'],
@@ -13,5 +17,5 @@ setup(
     license='LICENSE.txt',
     description= 'An open-source Python package for laboratory automation and analysis.',
     long_description=open('README.txt').read(),
-    install_requires=["numpy", "obspy","scipy", "matplotlib", "h5py", "obspyh5"]
+    install_requires=['numpy>1.0.0', 'obspy','scipy', 'matplotlib', 'h5py', 'obspyh5']
     )
