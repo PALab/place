4febdced0b3ce8d0a0e88e1f40e7b610ef47d85b
johjam <jami.johnson@auckland.ac.nz>
2014-06-16 12:03:58 +1200

    Update __init__.py

--- pypal/automate/osci_card/__init__.py
+++ pypal/automate/osci_card/__init__.py
@@ -1,7 +1,9 @@
 """
-This package allows the data acquisition with an Alazar oscilloscope card. 
+This package allows data acquisition with an Alazar oscilloscope card. 
 
 The controller module provides classes that can be used to initialize, configure and record. 
+An Alazar command library is required, and must be obtained from Alazar Tech when the card is purchased. The library should be saved as 
+AlazarCmd.py in the osci_card folder to be imported by the controller module.
 See the documentation of the controller module for more details(print osciCard.controller.__doc__). The remaining modules are used by the 
 controller module or are used for testing. 
 """
