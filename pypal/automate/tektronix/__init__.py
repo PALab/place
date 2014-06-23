a1d66f5e82bf3106b514035ae1830cc1cb314176
johjam <jami.johnson@auckland.ac.nz>
2014-06-23 14:21:13 +1200

    Update __init__.py

--- pypal/automate/tektronix/__init__.py
+++ pypal/automate/tektronix/__init__.py
@@ -6,5 +6,5 @@ See the documentation of the TEK_driver module for more details (print Tektronix
 @author: Jami L Johnson
 """
 from TEK_driver import TDS3014b
-
+import httplib2
 __all__ = ['tektronix']
