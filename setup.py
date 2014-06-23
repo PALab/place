8c4b174dfc27294dff761e42de2d1f73cbe8aeb9
johjam <jami.johnson@auckland.ac.nz>
2014-06-23 14:28:24 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     author_email='jami.johnson@auckland.ac.nz',
     packages=['pypal','pypal.analysis','pypal.automate','pypal.automate.osci_card','pypal.automate.polytec','pypal.automate.tektronix','pypal.automate.xps_control'],
     scripts=['bin/Scan.py','bin/example_PALplots.py'],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.txt',
     url='https://github.com/johjam/PyPal',
     description= 'An open-source Python package for laboratory automation and analysis.',
