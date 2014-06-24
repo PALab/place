d12592113df217cdb5d89280b901335e3f30fc08
johjam <jami.johnson@auckland.ac.nz>
2014-06-25 11:58:02 +1200

    Update __init__.py

--- pypal/automate/__init__.py
+++ pypal/automate/__init__.py
@@ -1,4 +1,4 @@
-import osci_card
+import osci_card #NOTE: this line must be commented if an alazar tech card has not been purchased (and the Alazar library is not installed) 
 import polytec
 import tektronix
 import xps_control
