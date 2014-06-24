e48b8c33ec1b97839df17cbd1c134e083db80b53
johjam <jami.johnson@auckland.ac.nz>
2014-06-25 11:14:54 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ except ImportError:
 setup(
     name='PyPAL',
     version='0.1.0',
-    author='Jami L. Johnson, Kasper van Wijk, Henrik tom Worden',
+    author='Jami L. Johnson, Henrik tom Worden, Kasper van Wijk,',
     author_email='jami.johnson@auckland.ac.nz',
     packages=['pypal','pypal.analysis','pypal.automate','pypal.automate.osci_card','pypal.automate.polytec','pypal.automate.tektronix','pypal.automate.xps_control'],
     scripts=['bin/Scan.py','bin/example_PALplots.py'],
-    license='LICENSE.txt',
+    license='GNU General Public License, Version 3 (LGPLv3)',
     url='https://github.com/johjam/PyPal',
     description= 'An open-source Python package for laboratory automation and analysis.',
     long_description=open('README.txt').read(),
