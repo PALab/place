d83610b5e6cf058e7337dbd417dad6326f41113c
johjam <jami.johnson@auckland.ac.nz>
2014-06-25 12:00:30 +1200

    Update __init__.py

--- pypal/__init__.py
+++ pypal/__init__.py
@@ -1,1 +1,8 @@
-# NOTE: in pypal/automate/osci_card/__init__.py, the line "import osci_card" must be removed or commented if an Alazar card is not used.  The module requires a C library from Alazar Tech that is required in order to use the driver module.
+'''
+NOTE: 
+In pypal/automate/osci_card/__init__.py, 
+the line "import osci_card" must be removed or 
+commented if an Alazar card is not used.  
+A C library from Alazar Tech must be installed
+in order to use the driver module.
+'''
