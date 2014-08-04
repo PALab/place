0018879798d58a5a5f46c88c63b0ffe068d565ed
johjam <jami.johnson@auckland.ac.nz>
2014-08-04 14:23:47 +1200

    Update controller.py

--- pypal/automate/osci_card/controller.py
+++ pypal/automate/osci_card/controller.py
@@ -818,25 +818,6 @@ class TriggeredContinuousController(AbstractTriggeredADMAController):
     def getApproximateDuration(self):
         return  int(float(self.samplesPerRecord) * self.recordsPerBuffer * self.buffersPerCapture / self.samplesPerSec)
 
-# TODO: get rid of these. These functions where used when bad data appeared at ends or beginning of some records. This should not happen anymore.
-#    def getTimesOfRecord(self):
-#        """
-#        generates a time value to each sample value in a record.
-#        
-#        The time 0 is given to the first postTriggerSample. Time values are spaced according to the sampling rate.
-#        """
-        #sec = float(self.samplesPerRecord) / self.samplesPerSec
-        #return np.linspace(-sec * float(self.preTriggerSamples) / self.samplesPerRecord, sec * float(self.postTriggerSamples - 17) / self.samplesPerRecord, self.samplesPerRecord - 16)
-
-#    def getTimesOfCapture(self):
-#        """
-#        generates a time value to each sample value in a capture.
-#        
-#        If the capture consists of multiple records, these time values are likely to be not the actual times of the measurement.
-#        The first sample has the time 0. Time values are spaced according to the sampling rate.
-#        """
-#        return np.linspace(0., float(self.samplesPerRecord * self.recordsPerCapture - 1 - 16 * self.recordsPerCapture) / self.samplesPerSec, self.samplesPerRecord * float(self.recordsPerCapture) - 16 * self.recordsPerCapture)
-
     def _getPreTriggerSamples(self):
         return self.preTriggerSamples
     
@@ -854,7 +835,6 @@ class TriggeredContinuousController(AbstractTriggeredADMAController):
         records = [data[i * self.samplesPerRecord:(i + 1) * self.samplesPerRecord] for i in range(self.recordsPerBuffer * self.channelCount)]
         for i, channel in enumerate(sorted(self.data.keys())): 
             for record in records[i * self.recordsPerBuffer:(i + 1) * self.recordsPerBuffer]:
-                #self.data[channel].append(list(self._processData(record, channel))[:-16])  # TODO:remove this. This line deletes the ends and beginnings of each record. It can be used when some records have bad data. However, THIS SHOULD NOT HAPPEN 
                 self.data[channel].append(list(self._processData(record, channel)))  
 
     def _setSizeOfCapture(self):
@@ -912,25 +892,6 @@ class TriggeredRecordingController(AbstractTriggeredADMAController):
         self.dependendFunctions = [self._setClock, self._setSizeOfCapture, self._prepareCapture]
         self.admaFlags = 0x1 | 0x0  
 
-# TODO: get rid of these. These functions where used when bad data appeared at ends or beginning of some records. This should not happen anymore.
-#    def getTimesOfRecord(self):
-#        """
-#        generates a time value to each sample value in a record.
-#        
-#        The time 0 is given to the first postTriggerSample. Time values are spaced according to the sampling rate.
-#        """
-#        sec = float(self.samplesPerRecord) / self.samplesPerSec
-#        return np.linspace(-sec * float(self.preTriggerSamples) / self.samplesPerRecord, sec * float(self.postTriggerSamples - 33) / self.samplesPerRecord, self.samplesPerRecord - 32)
-#
-#    def getTimesOfCapture(self):
-#        """
-#        generates a time value to each sample value in a capture.
-#        
-#        If the capture consists of multiple records, these time values are likely to be not the actual times of the measurement.
-#        The first sample has the time 0. Time values are spaced according to the sampling rate.
-#        """
-#        return np.linspace(0., float(self.samplesPerRecord * self.recordsPerCapture - 1 - 32 * self.recordsPerCapture) / self.samplesPerSec, self.samplesPerRecord * float(self.recordsPerCapture) - 32 * self.recordsPerCapture)
-
     def getApproximateDuration(self):
         return  int(float(self.samplesPerRecord) * self.recordsPerCapture / self.samplesPerSec)
 
@@ -952,7 +913,6 @@ class TriggeredRecordingController(AbstractTriggeredADMAController):
         for i, channel in enumerate(sorted(self.data.keys())): 
             for record in records[i::self.channelCount]:
                 self.data[channel].append(list(self._processData(record, channel)))  
-                #self.data[channel].append(list(self._processData(record, channel))[16:-16])  # TODO:remove this. This line deletes the ends and beginnings of each record. It can be used when some records have bad data. However, THIS SHOULD NOT HAPPEN 
 
     def _setSizeOfCapture(self):
         """
