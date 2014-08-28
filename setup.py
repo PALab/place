80b5d9eace42530c106f62f2787069ba1a02465c
johjam <jami.johnson@auckland.ac.nz>
2014-08-28 15:39:37 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.0',
     author='Jami L. Johnson, Henrik tom Worden, Kasper van Wijk,',
     author_email='jami.johnson@auckland.ac.nz',
-    packages=['pypal','pypal.analysis','pypal.automate','pypal.automate.osci_card','pypal.automate.polytec','pypal.automate.tektronix','pypal.automate.xps_control'],
+    packages=['pypal','pypal.analysis','pypal.automate','pypal.automate.osci_card','pypal.automate.polytec','pypal.automate.tektronix','pypal.automate.xps_control','pypal.automate.SRS'],
     scripts=['bin/Scan.py','bin/example_PALplots.py'],
     license='GNU General Public License, Version 3 (LGPLv3)',
     url='https://github.com/johjam/PyPal',
