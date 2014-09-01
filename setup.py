7aca4370ac5334645b341f77f63b8ebb97f7a229
Jami L Johnson <jami.johnson@auckland.ac.nz>
2014-09-01 15:46:46 +1200

    Name change

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='PyPAL',
+    name='PLACE',
     version='0.1.0',
     author='Jami L. Johnson, Henrik tom Worden, Kasper van Wijk,',
     author_email='jami.johnson@auckland.ac.nz',
-    packages=['pypal','pypal.analysis','pypal.automate','pypal.automate.osci_card','pypal.automate.polytec','pypal.automate.tektronix','pypal.automate.xps_control','pypal.automate.SRS'],
+    packages=['place','place.analysis','place.automate','place.automate.osci_card','place.automate.polytec','place.automate.tektronix','place.automate.xps_control','place.automate.SRS'],
     scripts=['bin/Scan.py','bin/example_PALplots.py'],
     license='GNU General Public License, Version 3 (LGPLv3)',
-    url='https://github.com/johjam/PyPal',
-    description= 'An open-source Python package for laboratory automation and analysis.',
+    url='https://github.com/johjam/PLACE',
+    description= 'An open-source Python package for laboratory automation, control, and experimentation.',
     long_description=open('README.txt').read(),
     install_requires=['numpy>1.0.0', 'obspy','scipy', 'matplotlib', 'h5py', 'obspyh5','pyserial']
     )
