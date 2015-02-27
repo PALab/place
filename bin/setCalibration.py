a94137f1fc822f407ee8de73c0ccb92c6ad0b1f4
johjam <jami.johnson@auckland.ac.nz>
2015-02-27 15:25:03 +1300

    Calibration file for picomotor mirror positioning.

---
+++ bin/setCalibration.py
@@ -0,0 +1,20 @@
+from place.automate.polytec.vibrometer import PolytecSensorHead
+from place.automate.scan.scanFunctions_Evan import initialize
+from place.automate.new_focus.Calibrate import getConversion, Position, getInverse
+
+
+def main():
+    # Sets calibration values when the scanning mirror is repositioned.
+    # Currently only for use with polytec sensor head.
+    [pico_controller, x_mot, y_mot] = initialize().PicomotorController()
+    inverse = getInverse()
+    print "Position mirror for auto-focus"
+    Position(x_mot, y_mot, inverse=inverse)
+    PolytecSensorHead().autofocusVibrometer()
+    focuslength = float(PolytecSensorHead().getFocus())
+    getConversion(x_mot, y_mot, focuslength=focuslength, method='manual', inverse=inverse)
+    pico_controller.close()
+    return
+
+if __name__ == '__main__':
+    main()
