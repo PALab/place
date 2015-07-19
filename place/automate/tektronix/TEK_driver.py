c82c15bc60ca6a2471cb490cffc7772525c9e701
Jami L Johnson <jami.johnson@auckland.ac.nz>
2015-07-20 11:18:02 +1200

    update tektronix header to obspy format

--- place/automate/tektronix/TEK_driver.py
+++ place/automate/tektronix/TEK_driver.py
@@ -15,6 +15,8 @@ import urllib
 import os
 import struct
 import datetime
+from obspy import read, Trace, UTCDateTime
+from obspy.core.trace import Stats
 
 try:
     import numpy as np
@@ -104,31 +106,34 @@ class TDS3014b:
         # Extract data and convert from string to integer value
         data = isf_data[data_loc:]
 
-        num_pts = int(header_dict['NR_PT'])
-        byte_order = header_dict['BYT_OR'] # 'MSB' or 'LSB'
-        if(byte_order == 'MSB'):
-            byte_order = '>'
-        else:
-            byte_order = '<'
+        stats = Stats()
+        stats.npts = int(header_dict['NR_PT'])
+        
         points = []
-        for i in range(0, num_pts*2, 2):
+        for i in range(0, stats.npts*2, 2):
             value = data[i:i+2] # as string
             converted = struct.unpack('%sh' % byte_order, value)[0]
             points.append(converted)
             
         # Optionally convert points to engineering units
         if(convert):
-            ymult = float(header_dict['YMULT'])
+            stats.calib = float(header_dict['YMULT'])
             try:
-                points = np.array(points) * ymult  # requires numpy
+                points = np.array(points) * stats.calib  # requires numpy
             except NameError:
                 # If numpy not available, use list instead.
                 p = []
                 for point in points:
-                    p.append(point * ymult)
+                    p.append(point * stats.calib)
                 points = p
 
-        return header_dict, points
+        stats.calib_unit = header_dict['YUNIT']
+        stats.delta = header_dict['XINCR']
+        stats.offset = header_dict['YOFF']
+        stats.comments = header_dict['WFID']
+        stats.channel = header_dict['WFID'][0:3]
+
+        return stats, points
 
     def getWaveform(self, channel=1, format=None):
         """
