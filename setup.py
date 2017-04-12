31fccb7aa8ec0815bb7d2b9f60e7815417b44389
Paul Freeman <pfre484@auckland.ac.nz>
2017-04-12 12:26:42 +1200

    bump to version 0.2.4

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
+''' setup configuration file'''
 from setuptools import setup, find_packages
 
 setup(
     name='place',
-    version='0.2.2',
+    version='0.2.4',
     author='Jami L. Johnson, Henrik tom Worden, Kasper van Wijk,',
     author_email='jami.johnson@auckland.ac.nz',
     packages=find_packages(),
     scripts=['bin/example_DS345.py',
-        'bin/example_oscicard_AcquireInstancesAfterCommand.py',
-        'bin/example_oscicard_AverageMultipleRecords.py',
-        'bin/example_QuantaRay.py',
-        'bin/example_tektronix.py',
-        'bin/xps_demo.py',
-        'bin/xps_exampleUsageController.py'],
+             'bin/example_oscicard_AcquireInstancesAfterCommand.py',
+             'bin/example_oscicard_AverageMultipleRecords.py',
+             'bin/example_QuantaRay.py',
+             'bin/example_tektronix.py',
+             'bin/xps_demo.py',
+             'bin/xps_exampleUsageController.py'],
     license='GNU General Public License, Version 3 (LGPLv3)',
     classifiers=[
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Science/Research',
         'Development Status :: 3 - Alpha',
         'Operating System :: POSIX :: Linux',
         'Topic :: Scientific/Engineering :: Physics'],
     url='https://github.com/PALab/place',
-    description= 'An open-source Python package for laboratory automation, control, and experimentation.',
+    description=('An open-source Python package for laboratory automation, ' +
+                 'control, and experimentation.'),
     long_description=open('README.md').read(),
     entry_points={'console_scripts':[
         'place_scan = place.scripts.scan:main',
