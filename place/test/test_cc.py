c1c9d83460ee29ed557bc13d97a72681e608548f
Paul Freeman <email.paul.freeman@gmail.com>
2017-05-01 12:09:40 +1200

    updated to support unittest module

--- place/test/test_cc.py
+++ place/test/test_cc.py
@@ -1,23 +1,26 @@
-'''
-Test for the Continuous Controller
-'''
-from warnings import warn
+"""Test for the Continuous Controller"""
+from unittest import TestCase, main
 
 from place.automate.osci_card.c_controller import ContinuousController
 from place.alazartech.atsapi import SAMPLE_RATE_100KSPS, CHANNEL_A
 
-try:
-    control = ContinuousController()
-except Exception: #pylint: disable=broad-except
-    warn('No ATS card detected. Skipping controller test.')
-    exit()
-try:
-    control.create_input()
-except RuntimeError:
-    warn('This card does not support asychronous ADMA. Skipping test.')
-    exit()
-control.setSampleRate(SAMPLE_RATE_100KSPS)
-control.setCaptureDurationTo(1)
-control.start_capture()
-control.readData()
-data = control.getDataAtOnce(CHANNEL_A)
+class TestCC(TestCase):
+    """Continuous Controller test class"""
+    def test_continuous_controller(self):
+        """Basic test"""
+        try:
+            control = ContinuousController()
+        except Exception: #pylint: disable=broad-except
+            self.skipTest('No ATS card detected. Skipping controller test.')
+        try:
+            control.create_input()
+        except RuntimeError:
+            self.skipTest('This card does not support asychronous ADMA. Skipping test.')
+        control.setSampleRate(SAMPLE_RATE_100KSPS)
+        control.setCaptureDurationTo(1)
+        control.start_capture()
+        control.readData()
+        control.getDataAtOnce(CHANNEL_A)
+
+if __name__ == '__main__':
+    main(verbosity=2, buffer=True)
