7faf7ad643d0f26c276ec5b86817fe4faad2e46a
Paul Freeman <email.paul.freeman@gmail.com>
2017-12-01 11:29:51 +1300

    add timeout to connection

--- place/plugins/sr850_amp/sr850_driver.py
+++ place/plugins/sr850_amp/sr850_driver.py
@@ -18,7 +18,8 @@ class SR850Driver:
                            baudrate=9600,
                            bytesize=serial.EIGHTBITS,
                            parity=serial.PARITY_NONE,
-                           stopbits=serial.STOPBITS_TWO) as connection:
+                           stopbits=serial.STOPBITS_TWO,
+                           timeout=10) as connection:
             connection.write(bytes(cmd + '\r', 'ascii'))
             sleep(0.1)
 
