acd21556073f74ee08a0e54a4dc0c6f6f9f9cdc6
Jonathan Simpson <jsim921@aucklanduni.ac.nz>
2023-05-23 10:43:53 +1200

    Updating serial_search

--- place/serial_search.py
+++ place/serial_search.py
@@ -21,7 +21,7 @@ def get_available_serial_ports():
             A list of the serial ports available on the system
     """
     if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
+        ports = ['COM{}'.format(i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
         # this excludes your current terminal "/dev/tty"
         ports = glob.glob('/dev/tty[A-Za-z]*')
