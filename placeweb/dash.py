6041d0a54f24534fb90f5bfbfaa3ea2b579aa4e2
Paul Freeman <paul@omiq.ai>
2024-02-28 08:54:58 +1300

    outline first dash page

--- placeweb/dash.py
+++ placeweb/dash.py
@@ -1,26 +1,67 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
-import pandas as pd
+import dash_bootstrap_components as dbc
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
-)
+app = Dash(external_stylesheets=[dbc.themes.SPACELAB])
 
-app = Dash(__name__)
+table_header = [
+    html.Thead(
+        html.Tr(
+            [
+                html.Th("Status"),
+                html.Th("Timestamp"),
+                html.Th("Title"),
+                html.Th("Comments"),
+                html.Th("Results"),
+                html.Th("Repeat"),
+                html.Th("Download"),
+                html.Th("Delete"),
+            ]
+        )
+    )
+]
 
 app.layout = html.Div(
     [
-        html.H1(children="Title of Dash App", style={"textAlign": "center"}),
-        dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
-        dcc.Graph(id="graph-content"),
+        dbc.Nav(
+            [
+                dbc.NavItem(dbc.NavLink("PLACE 0.10.0", href="/")),
+                dbc.NavItem(
+                    dbc.NavLink(
+                        "Install",
+                        href="https://anaconda.org/freemapa/place",
+                        external_link=True,
+                    )
+                ),
+                dbc.NavItem(
+                    dbc.NavLink(
+                        "Documentation",
+                        href="http://127.0.0.1:8050/documentation/index.html",
+                        external_link=True,
+                    )
+                ),
+                dbc.NavItem(
+                    dbc.NavLink(
+                        "Source",
+                        href="https://www.github.com/palab/place",
+                        external_link=True,
+                    )
+                ),
+                dbc.NavItem(
+                    dbc.NavLink(
+                        "PAL",
+                        href="https://pal.blogs.auckland.ac.nz/",
+                        external_link=True,
+                    )
+                ),
+            ],
+            navbar=True,
+        ),
+        html.H3(children="Experiment History", style={"textAlign": "left"}),
+        dbc.NavLink("New Experiment", href="/new"),
+        dbc.Table(table_header, bordered=True),
     ]
 )
-
-
-@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x="year", y="pop")
 
 
 def start():
